9ca5a4e3bbf5cc7496614d1b534a135041771274
kshelp <kshelp@naver.com>
2021-08-03 17:35:16 +0900

    .

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,17 +1,14 @@
 package com.example.demo;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.example.demo.dto.BoardDto;
 import com.example.demo.dto.UserDto;
 import com.example.demo.service.BoardService;
 import com.example.demo.service.UserService;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
@@ -22,28 +19,30 @@ public class DemoApplicationTests {
 	private BoardService bService;
 
 	@Test
+	public void userTest() {
+		UserDto user = new UserDto();
+		user.setUserId("test11");
+		user.setUserPw("test11");
+		user.setUserName("테스트11");
+		user.setUserGender("남");
+		user.setUserEmail("test11@gmail.com");
+		uService.join(user);
+		System.out.println(uService.getUser("test11"));
+		System.out.println("/*** 로그인 결과 : " + uService.login("test11", "test11"));
+	}
+	
+	@Test
 	public void boardTest() {
 		BoardDto board = new BoardDto();
-		board.setWriter("test1");
+		board.setWriter("test11");
 		board.setPassword("1234");
-		board.setTitle("test1");
-		board.setContent("test1");
+		board.setTitle("test11");
+		board.setContent("test11");
 		bService.write(board);
 
 		for (BoardDto b : bService.getBoardList())
 			System.out.println("/*** " + b);
 	}
 
-	//@Test
-	public void userTest() {
-		UserDto user = new UserDto();
-		user.setUserId("test12");
-		user.setUserPw("test12");
-		user.setUserName("테스트12");
-		user.setUserGender("남");
-		user.setUserEmail("test12@gmail.com");
-		uService.join(user);
-		System.out.println(uService.getUser("test12"));
-		System.out.println("로그인 결과 : " + uService.login("test12", "test12"));
-	}
+	
 }
